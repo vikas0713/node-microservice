8a502d7b6aaee15f528c68e10ff3d69cadb9716e
Vikas Verma <vikas.verma@helloalfred.com>
2022-02-26 12:38:41 +0530

    Initial commit

---
+++ users-service/bable.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: [
+        "module-resolver",
+        {
+            "alias": {
+                "#root": "./src"
+            }
+        }
+    ],
+    presets: [
+        "@babel/preset-env",
+        {
+            targets: {
+                node: "current"
+            }
+        }
+    ]
+};
